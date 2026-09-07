004837de9e7216bb711abeef7605d63daeb85ed4
Meta <beatsalex0@gmail.com>
2026-09-08 01:36:32 +0800

    build: bump versionCode to 50

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
     defaultConfig {
         applicationId = "com.castle.sefirah"
 
-        versionCode = 49
+        versionCode = 50
         versionName = "3.0.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
